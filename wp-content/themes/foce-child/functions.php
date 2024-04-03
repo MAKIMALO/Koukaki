685aca28855424c8d7ff4bfbad352cea48dc6171
Marlene Audic <rondineau.m@gmail.com>
2024-04-03 16:33:58 +0200

    Etape 1 : ajout du scroll sur les titres

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -3,12 +3,8 @@
     function theme_enqueue_styles() {
         wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
         wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/sass/theme.css', array(), filemtime(get_stylesheet_directory() . '/sass/theme.css') );
-    }
-
-    add_action('wp_enqueue_scripts', 'theme_enqueue_script');
-
-    function theme_enqueue_script() {
-        wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/script.js', array(), '1.0', true );
+    
+        wp_enqueue_script( 'custom-script', get_stylesheet_directory_uri() . '/script.js', array(), '1.2', true );
     }
 ?>
 
--- wp-content/themes/foce-child/script.js
+++ wp-content/themes/foce-child/script.js
@@ -1,21 +1,18 @@
-let observer = new IntersectionObserver(observables => {
-    for(let observable of observables){
-        if(observable.intersectionRatio > 0.5){
-            observable.target.classList.remove("title_hidden");
-        } else {
-            observable.target.classList.add("title_hidden");
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+
+        if (entry.isIntersecting) {
+            entry.target.classList.add("title_animation");
+            return;
         }
-    }
-}, {
-    threshold: [0.5]
+
+        entry.target.classList.remove("title_animation");
+    });
 });
 
-const titles_h2 = document.querySelectorAll("h2 span");
-const titles_h3 = document.querySelectorAll("h3 span");
+const titles = document.querySelectorAll("h2 span");
 
-for(let title_h2 of titles_h2) {
-    title_h2.classList.add("title_hidden");
-    observer.observe(title_h2)
-}
-
+titles.forEach((title) => {
+    observer.observe(title);
+});
 
