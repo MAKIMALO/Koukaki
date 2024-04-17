6ff8f40a31d2ef254e29486cd5fbf5be391cd201
Marlene Audic <rondineau.m@gmail.com>
2024-04-17 16:10:46 +0200

    Responsive en cours

--- wp-content/themes/foce-child/header.php
+++ wp-content/themes/foce-child/header.php
@@ -26,7 +26,7 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'foce' ); ?></a>
 
 	<header id="masthead" class="site-header">
-        <div class="top-site-title">
+        <nav class="top-site-title">
             <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
             <a href="#" id="openBtn">
                 <span class="burger-icon">
@@ -36,27 +36,29 @@
                 </span>
             </a>
             <a id="closeBtn" href="#" class="close">x</a>
-        </div>
-        <div id="mytopnav" class="topnav">
-            <ul>
-                <img class="img_logo_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo_burger.png'; ?>" alt="logo Fleurs d'oranger & chats errants"> 
-                <li class="anim_title_1"><a href="#story">Histoire</a></li>
-                <li class="anim_title_1"><a href="#characters">Personnages</a></li>
-                <li class="anim_title_1"><a href="#place">Lieu</a></li>
-                <li class="anim_title_1"><a href="#studio">Studio Koukaki</a></li>
-                <img class="logo_studio_koukaki" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Studio_Koukaki.png'; ?>" alt="Logo du studio Koukaki"> 
-            </ul>
-            <div class="img_burger">
-                <img class="orchid_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/orchid.png'; ?>" alt="Image d'une orchidée violette">
-                <img class="sunflower_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/sunflower.png'; ?>" alt="Image d'une marguerite blanche">  
-                <img class="flower_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/flower.png'; ?>" alt="Image d'une fleur orange">  
-                <img class="random_flower_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/random_flower.png'; ?>" alt="Image d'une fleur rose">  
-                <img class="hibiscus_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/hibiscus.png'; ?>" alt="Image d'une fleur d'hibiscus orange">  
-                <img class="blue_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/blue_cat.png'; ?>" alt="Image d'un chat bleu"> 
-                <img class="orange_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/orange_cat.png'; ?>" alt="Image d'un chat orange">
-                <img class="black_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/black_cat.png'; ?>" alt="Image d'un chat noir">
+        </nav>
+        <section id="mytopnav" class="topnav">
+            <div class="menu_burger">
+                <ul>
+                    <img class="img_logo_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/logo_burger.png'; ?>" alt="logo Fleurs d'oranger & chats errants"> 
+                    <li class="anim_title_1"><a href="#story">Histoire</a></li>
+                    <li class="anim_title_1"><a href="#characters">Personnages</a></li>
+                    <li class="anim_title_1"><a href="#place">Lieu</a></li>
+                    <li class="anim_title_1"><a href="#studio">Studio Koukaki</a></li>
+                    <img class="logo_studio_koukaki" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/Studio_Koukaki.png'; ?>" alt="Logo du studio Koukaki"> 
+                </ul>
+                <div class="img_burger">
+                    <img class="orchid_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/orchid.png'; ?>" alt="Image d'une orchidée violette">
+                    <img class="sunflower_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/sunflower.png'; ?>" alt="Image d'une marguerite blanche">  
+                    <img class="flower_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/flower.png'; ?>" alt="Image d'une fleur orange">  
+                    <img class="random_flower_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/random_flower.png'; ?>" alt="Image d'une fleur rose">  
+                    <img class="hibiscus_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/hibiscus.png'; ?>" alt="Image d'une fleur d'hibiscus orange">  
+                    <img class="blue_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/blue_cat.png'; ?>" alt="Image d'un chat bleu"> 
+                    <img class="orange_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/orange_cat.png'; ?>" alt="Image d'un chat orange">
+                    <img class="black_cat_burger" src="<?php echo get_stylesheet_directory_uri() . '/assets/images/black_cat.png'; ?>" alt="Image d'un chat noir">
+                </div>
             </div>
-        </div>
+        </section>  
             
 	</header><!-- #masthead -->
 
