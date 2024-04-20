2af41d22f4f21342357fd47b1c18601cedb4bd88
Marlene Audic <rondineau.m@gmail.com>
2024-04-20 09:16:23 +0200

    Responsive en cours

--- wp-content/themes/foce-child/template-parts/oscars.php
+++ wp-content/themes/foce-child/template-parts/oscars.php
@@ -17,12 +17,7 @@
             <img class="image_orchid" src="<?php echo $image_orchid; ?>" alt="Image orchidée">
         </div>
         <div class="nomination">
-            <div class="text_nomination">
-                <?php
-                $background_image_orange = get_theme_file_uri("assets/images/background_image_orange.png");
-                ?>
-                <img class="background_image_orange" src="<?php echo $background_image_orange; ?>" alt="Image de fond d'écran orange">
-
+            <div class="text_nomination" style="background-image: url('<?php echo get_theme_file_uri("assets/images/background_orange_oscars.png"); ?>')">
                 <p>Fleurs d'oranger & chats errants<br>est nominé aux Oscars Short<br>Film Animated de 2022 !</p>
             </div>
             <div class="logo_nomination">
